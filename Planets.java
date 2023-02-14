1a8f85340e8594b1850e8ecf9d559fcc2eecc10b
MatthewWKurtz <matthewwwkurtz@gmail.com>
2023-02-14 09:46:03 -0500

    Plantes

---
+++ Planets.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Planets
+{
+    enum planets {MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE}; //Not Pluto
+    
+    public static void main(String[]args)
+    {
+        Scanner kybrd = new Scanner(System.in);
+        String input = "";
+
+    
+        while(!input.equalsIgnoreCase("Quit"))
+        {
+            System.out.print("Name a planet (Quit to Quit): ");
+            input = kybrd.nextLine();
+
+            try
+            {
+                System.out.println("That planet is in position " + (planets.valueOf(input.toUpperCase()).ordinal()+1) + " of the solar system.");
+            }
+            catch(IllegalArgumentException e)
+            {
+                System.out.println("Invalid Planet");
+            }
+
+            System.out.println();
+        }
+    }
+}
