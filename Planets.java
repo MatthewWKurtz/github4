09532bb85a527d8c0890fd6f0e356fe1fa286835
Jacob Flasko <115510621+JacobFlasko@users.noreply.github.com>
2023-02-14 09:55:40 -0500

    Words of Pluto
    I added plutos words

--- Planets.java
+++ Planets.java
@@ -6,6 +6,7 @@ public class Planets
     
     public static void main(String[]args)
     {
+        String wordsOfPluto("I am a planet");
         Scanner kybrd = new Scanner(System.in);
         String input = "";
 
